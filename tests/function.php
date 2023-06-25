1551744e02aaa679db7433342cff8d6222ebf5f7
ZX <903464207@qq.com>
2023-06-25 14:52:31 +0800

    1

--- tests/function.php
+++ tests/function.php
@@ -19,12 +19,5 @@ if (!function_exists('pp')) {
     }
 
 }
-if (!function_exists('is_json')) {
 
-    function is_json($string) {
-        @json_decode($string);
-        return (json_last_error() == JSON_ERROR_NONE);
-    }
-
-}
 
