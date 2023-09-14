34847c42c5923685ce837abac9ebacf0ef607b80
ZX <903464207@qq.com>
2023-09-14 13:30:41 +0800

    新增goravel模版

--- tests/test.php
+++ tests/test.php
@@ -11,14 +11,16 @@ include_once './../src/Generator/LaravelSnake.php';
 include_once './../src/Generator/LaravelSoftDelZx.php';
 include_once './../src/Generator/WebmanCamel.php';
 include_once './../src/Generator/WebmanSnake.php';
+include_once './../src/Generator/GoravelSnake.php';
 
-use ZX\Generator\WebmanCamel;
-use ZX\Generator\WebmanSnake;
 use ZX\Tool\File;
 use ZX\Tool\MysqlOperation;
 use ZX\Generator\LaravelCamel;
 use ZX\Generator\LaravelSnake;
 use ZX\Generator\LaravelSoftDelZx;
+use ZX\Generator\WebmanCamel;
+use ZX\Generator\WebmanSnake;
+use ZX\Generator\GoravelSnake;
 
 $param = [
     'type' => 'mysql',
@@ -36,40 +38,47 @@ MysqlOperation::setConnection($param);
  * 非软删除版本 变量蛇形命名模版
  */
 
-File::deldir('./' . LaravelSnake::getAppPath() . DIRECTORY_SEPARATOR);
-LaravelSnake::generatorAllTable();
-LaravelSnake::generatorAllRouter();
+//File::deldir('./' . LaravelSnake::getAppPath() . DIRECTORY_SEPARATOR);
+//LaravelSnake::generatorAllTable();
+//LaravelSnake::generatorAllRouter();
 
 
 /*
  * 非软删除版本 变量驼峰命名模版
  */
 
-File::deldir('./' . LaravelCamel::getAppPath() . DIRECTORY_SEPARATOR);
-LaravelCamel::generatorAllTable();
-LaravelCamel::generatorAllRouter();
+//File::deldir('./' . LaravelCamel::getAppPath() . DIRECTORY_SEPARATOR);
+//LaravelCamel::generatorAllTable();
+//LaravelCamel::generatorAllRouter();
 
 /*
  * 软删除版本特殊代码，变量蛇形命名模版
  */
 
-File::deldir('./' . LaravelSoftDelZx::getAppPath() . DIRECTORY_SEPARATOR);
-LaravelSoftDelZx::generatorAllTable();
-LaravelSoftDelZx::generatorAllRouter();
+//File::deldir('./' . LaravelSoftDelZx::getAppPath() . DIRECTORY_SEPARATOR);
+//LaravelSoftDelZx::generatorAllTable();
+//LaravelSoftDelZx::generatorAllRouter();
 
 /*
  * 非软删除版本 变量驼峰命名模版
  */
 
-File::deldir('./' . WebmanCamel::getAppPath() . DIRECTORY_SEPARATOR);
-WebmanCamel::generatorAllTable();
-WebmanCamel::generatorAllRouter();
+//File::deldir('./' . WebmanCamel::getAppPath() . DIRECTORY_SEPARATOR);
+//WebmanCamel::generatorAllTable();
+//WebmanCamel::generatorAllRouter();
 
 /*
  * 非软删除版本 变量蛇形命名模版
  */
 
-File::deldir('./' . WebmanSnake::getAppPath() . DIRECTORY_SEPARATOR);
-WebmanSnake::generatorAllTable();
-WebmanSnake::generatorAllRouter();
+//File::deldir('./' . WebmanSnake::getAppPath() . DIRECTORY_SEPARATOR);
+//WebmanSnake::generatorAllTable();
+//WebmanSnake::generatorAllRouter();
 
+/*
+ * 非软删除版本 go 变量蛇形命名模版
+ */
+
+File::deldir('./' . GoravelSnake::getAppPath() . DIRECTORY_SEPARATOR);
+GoravelSnake::generatorAllTable();
+GoravelSnake::generatorAllRouter();
